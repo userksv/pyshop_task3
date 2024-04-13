e028346f09b4de6cffb649389ede050e932fbf47
Mik <kimsergkorea@gmail.com>
2024-04-13 20:54:10 +0900

    debug false

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
