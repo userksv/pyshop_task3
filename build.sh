fb36cd3f5a642a9a66c5a0f5ee2d1f0b9ee4eda7
Mik <kimsergkorea@gmail.com>
2024-04-14 13:57:54 +0900

    createsuperuser

--- build.sh
+++ build.sh
@@ -11,3 +11,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
